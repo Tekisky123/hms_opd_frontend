fe883af8a4815a06a08cdde2d260772109428f61
Tohid4641 <dev.tauhid@gmail.com>
2025-10-25 21:30:36 +0530

    api token not setting

--- src/utils/api.js
+++ src/utils/api.js
@@ -9,31 +9,51 @@ const api = axios.create({
   },
 })
 
-// Request interceptor to add token
+
+// Add a request interceptor to include token
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token') 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
+    const token = localStorage.getItem("token");
+
+    if (token && config.headers) {
+      // Correct way: mutate headers, don’t replace
+      config.headers.set
+        ? config.headers.set("Authorization", `Bearer ${token}`)
+        : (config.headers["Authorization"] = `Bearer ${token}`);
     }
-    return config
+
+    return config;
   },
   (error) => {
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
-)
+);
 
-// Response interceptor for error handling
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem('token')
-      localStorage.removeItem('user')
-      window.location.href = '/'
-    }
-    return Promise.reject(error)
-  }
-)
+// Request interceptor to add token
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem('token') 
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`
+//     }
+//     return config
+//   },
+//   (error) => {
+//     return Promise.reject(error)
+//   }
+// )
+
+// // Response interceptor for error handling
+// api.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     if (error.response?.status === 401) {
+//       localStorage.removeItem('token')
+//       localStorage.removeItem('user')
+//       window.location.href = '/'
+//     }
+//     return Promise.reject(error)
+//   }
+// )
 
 export default api
